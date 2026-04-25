5b51060815acfb2152e5318e162bb08e2c45bf9a
WooJin Kong <kongjak@kongjak.dev>
2026-04-25 20:14:07 +0900

    fix: Dining data load failure indicators

--- feature/dining/src/main/java/in/koreatech/koin/feature/dining/ui/diningdetail/DiningViewModel.kt
+++ feature/dining/src/main/java/in/koreatech/koin/feature/dining/ui/diningdetail/DiningViewModel.kt
@@ -133,6 +133,8 @@ class DiningViewModel @Inject constructor(
                     }
                     .onFailure {
                         _dining.value = listOf()
+                        _isLoading.value = false
+                        _isDiningRefreshing.value = false
                     }
             }
         }
